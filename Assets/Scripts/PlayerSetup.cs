61a4d728beebcbc42160be177eba8ce6ed9b34d1
Michael Richardson <michael@richardsons.co.za>
2020-03-23 11:57:13 +0000

    Added base networking.

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private Camera sceneCamera;
+
+    void Start(){
+        if(!isLocalPlayer){
+            for(int ii = 0; ii < componentsToDisable.Length; ii++){
+                componentsToDisable[ii].enabled = false;
+            }
+        }
+        else{
+            sceneCamera = Camera.main;
+            if(sceneCamera != null){
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable() {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
